fb8cd498676253d858f31e653405cae0ba85a342
Zirk23 <120124162+Zirk23@users.noreply.github.com>
2023-05-22 11:26:47 +0800

    changes

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ function checklogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if(username=="admin" && password=="admin123") {
-        document.location.href = "dashboard.html";
+        document.location.href = "db.html";
     } else {
         document.getElementById("message").innerHTML = "Incorrect username or password (for admins/authorized personnel only)";
     }
